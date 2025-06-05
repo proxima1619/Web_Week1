cd15750bc71eb145de3ba8b3d79887656a807600
proxima1619 <proxima1619@gmail.com>
2025-06-05 15:39:14 +0900

    메인 페이지 구현
    로그인 상태 확인 및 게시글 목록 조회 기능 구현

--- src/index.php
+++ src/index.php
@@ -1,3 +1,8 @@
 <?php
-    phpinfo();
+    session_start();
+    $mysqli = new mysqli("db", "root", "rootpass", "board_db");
+    $result = $mysqli->query("SELECT posts.id, title, username FROM posts JOIN users ON posts.user_id = users.id ORDER BY posts.id DESC");
+    while($row = $result->fetch_assoc()) {
+        echo "<a href='read.php?id={$row['id']}'>{$row['title']} - {$row['username']}</a><br>";
+}
 ?>
